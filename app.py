253f8b85a9dea6489fac0b85b3a59762e3d5f060
randor12 <rynicholas@vt.edu>
2021-02-26 19:19:37 -0500

    starter code

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    """
+    Get the index page 
+    """
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        company = request.form['company']
+        
+        if company is not None:
+            print('Searching for company: %s' % company)
+            # Gather information on the stock for this company here 
+        else:
+            print("Could not find that company")
+        
+        return redirect(url_for('display'))
+
+@app.rout('/display', methods=['POST', 'GET'])
+def display():
+    """
+    Display findings on the stock on this page 
+    """
+    
+    if request.method == 'GET':
+        return render_template('results.html')
+    else:
+        company = request.form['company']
+        
+        # perform action with the new company 
+        
+        return render_template('results.html')
